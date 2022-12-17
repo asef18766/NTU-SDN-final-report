e8b8b29cf37609f91bce4c39c5b6067dededdbdd
jimmy0412 <k56ipu5050@gmail.com>
2022-12-17 16:29:32 +0800

    try to fix gef gdb & add debug arg to run_client.sh and run_server.sh

--- SSL_vuln/run_client.sh
+++ SSL_vuln/run_client.sh
@@ -6,8 +6,14 @@ if [[  $# -lt 1 ]]
         exit 1 ; 
 fi 
 
-
-
 ## client
+
 (cd ./client && ./create_client_cert.sh ${1})
-$PATH_TO_OPENSSL/openssl s_client -connect localhost:9001 -chainCAfile "client/${1}.pem" -cert client/leaf.pem -key client/leaf.key -state
+
+if [ "$2" ==  "debug" ]
+then
+    gdb --args $PATH_TO_OPENSSL/openssl s_client -connect localhost:9001 -chainCAfile "client/${1}.pem" -cert client/leaf.pem -key client/leaf.key
+else
+    $PATH_TO_OPENSSL/openssl s_client -connect localhost:9001 -chainCAfile "client/${1}.pem" -cert client/leaf.pem -key client/leaf.key -state
+
+fi 
--- SSL_vuln/run_server.sh
+++ SSL_vuln/run_server.sh
@@ -8,6 +8,11 @@ fi
 
 ## server
 (cd ./server && ./create_server_cert.sh ${1})
-$PATH_TO_OPENSSL/openssl s_server  -cert server/leaf.pem -key server/leaf.key -cert_chain "server/${1}.pem" -accept 9001 -verify 2 -state
 
+if [ "$2" ==  "debug" ]
+then
+    gdb --args $PATH_TO_OPENSSL/openssl s_server  -cert server/leaf.pem -key server/leaf.key -cert_chain "server/${1}.pem" -accept 9001 -verify 2 
+else
+    $PATH_TO_OPENSSL/openssl s_server  -cert server/leaf.pem -key server/leaf.key -cert_chain "server/${1}.pem" -accept 9001 -verify 2 -state
+fi
 
