50f50c4a5f0cd1b0edf32b9c8862ecc2c2e9a6c7
jimmy0412 <k56ipu5050@gmail.com>
2022-12-28 14:35:49 +0800

    add gdb script & change shell script usage

--- SSL_vuln/run_client.sh
+++ SSL_vuln/run_client.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [[  $# -lt 1 ]]
     then 
-        echo "usage ./run.sh <client_mode>"
+        echo "usage ./run.sh <client_mode> <debug>"
         echo "mode: ca , vuln"
         exit 1 ; 
 fi 
@@ -12,7 +12,7 @@ fi
 
 if [ "$2" ==  "debug" ]
 then
-    gdb --args $PATH_TO_OPENSSL/openssl s_client -connect localhost:9001 -chainCAfile "client/${1}.pem" -cert client/leaf.pem -key client/leaf.key
+    gdb -x gdb_script --args $PATH_TO_OPENSSL/openssl s_client -connect localhost:9001 -chainCAfile "client/${1}.pem" -cert client/leaf.pem -key client/leaf.key
 else
     $PATH_TO_OPENSSL/openssl s_client -connect localhost:9001 -chainCAfile "client/${1}.pem" -cert client/leaf.pem -key client/leaf.key -state
 
--- SSL_vuln/run_server.sh
+++ SSL_vuln/run_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [[  $# -lt 1 ]]
     then 
-        echo "usage ./run.sh <server_mode>"  
+        echo "usage ./run.sh <server_mode> <debug>"  
         echo "mode: ca , vuln"
         exit 1 ; 
 fi 
@@ -11,7 +11,7 @@ fi
 
 if [ "$2" ==  "debug" ]
 then
-    gdb --args $PATH_TO_OPENSSL/openssl s_server  -cert server/leaf.pem -key server/leaf.key -cert_chain "server/${1}.pem" -accept 9001 -verify 2 
+    gdb -x gdb_script --args $PATH_TO_OPENSSL/openssl s_server  -cert server/leaf.pem -key server/leaf.key -cert_chain "server/${1}.pem" -accept 9001 -verify 2 
 else
     $PATH_TO_OPENSSL/openssl s_server  -cert server/leaf.pem -key server/leaf.key -cert_chain "server/${1}.pem" -accept 9001 -verify 2 -state
 fi
